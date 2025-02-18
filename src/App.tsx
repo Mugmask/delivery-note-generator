cc1e61579d0c87b51fa1fce921a8a5e605ab4acd
mug <franciscoperezugalde@gmail.com>
2025-02-18 15:43:10 -0300

    chore: initialize project with Vite and ShadCN

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button';
+import { ThemeToggle } from '@/components/mode-toggle';
+
+function App() {
+  return (
+    <div>
+      <ThemeToggle />
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
+
